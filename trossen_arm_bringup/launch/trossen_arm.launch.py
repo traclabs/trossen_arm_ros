93f115c1ad3932cbaff5e60d31e3f76371c13d02
Lucas Davis <lucas.davis@traclabs.com>
2025-07-24 13:58:01 +0000

    name fix

--- trossen_arm_bringup/launch/trossen_arm.launch.py
+++ trossen_arm_bringup/launch/trossen_arm.launch.py
@@ -197,7 +197,7 @@ def generate_launch_description():
                     LaunchConfiguration('robot_model'),
                     ]), '.urdf.xacro ',
                 'use_world_frame:=', LaunchConfiguration('use_world_frame'), ' ',
-                'arm_variant:=', LaunchConfiguration('arm_variant'), ' ',
+                'variant:=', LaunchConfiguration('arm_variant'), ' ',
                 'arm_side:=', LaunchConfiguration('arm_side'), ' ',
                 'ros2_control_hardware_type:=', LaunchConfiguration('ros2_control_hardware_type'), ' ',
                 'ip_address:=', LaunchConfiguration('ip_address'), 
